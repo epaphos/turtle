144280e41b9eed4061af32732383815dcc56eb85
Björn Lexell <bjorn.lexell@gmail.com>
2013-04-15 14:13:02 +0200

    Added question class

---
+++ TurtleStack/src/com/example/turtlestack/Question.java
@@ -0,0 +1,14 @@
+package com.example.turtlestack;
+
+public class Question {
+	
+	private String title;
+	
+	public Question(String title) {
+		this.title = title; 
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+}
