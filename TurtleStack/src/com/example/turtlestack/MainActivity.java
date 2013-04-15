f4601c20592fd7c452311f0a088f81d6db53b7a7
Björn Lexell <bjorn.lexell@gmail.com>
2013-04-15 15:47:17 +0200

    Database setup

--- TurtleStack/src/com/example/turtlestack/MainActivity.java
+++ TurtleStack/src/com/example/turtlestack/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.turtlestack;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.Cursor;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,13 +12,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v("troll", "Hej");
+		TurtleSQLiteDatabase db = new TurtleSQLiteDatabase(this);
+		Log.v("question", db.getQuestion(8414075).getTitle());
 	}
-
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public Question getQuestion(int id) {
+		TurtleSQLiteDatabase db = new TurtleSQLiteDatabase(this);
+		db.createDatabase();
+		db.openDataBase();
+		Cursor cursor = db.query("posts", new String[] {"title"}, "id = ?", 
+				new String [] {String.valueOf(id)}, null, null, null, null);
+		return new Question(cursor.getString(0));
+		 
+	}
+	
 
 }
--- TurtleStack/src/com/example/turtlestack/TurtleSQLiteDatabase.java
+++ TurtleStack/src/com/example/turtlestack/TurtleSQLiteDatabase.java
@@ -1,20 +1,66 @@
 package com.example.turtlestack;
 
+import java.io.*;
+
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TurtleSQLiteDatabase extends SQLiteOpenHelper {
 
-	private static String DB_PATH="/databases/";
+	private static String DB_PATH="";
 	private static String DB_NAME="so.sqlite";
+	Context context;
 	
 	private SQLiteDatabase database;
 	
 	
 	public TurtleSQLiteDatabase (Context context) {
 		super(context, DB_NAME, null, 1);
+		DB_PATH = context.getFilesDir().getPath() + context.getPackageName() + "/databases/";
+		this.context = context;
+	}
+	
+	public void createDatabase() {
+		if(!databaseExists()) {
+			this.getReadableDatabase();
+			this.close();
+			try {
+				copyDatabase();
+			}
+			catch(IOException exception){
+				throw new Error("ErrorCopyingDatabase");
+			}
+		}	
+	}
+	
+	private boolean databaseExists() {
+		File file = new File(DB_PATH + DB_NAME);
+		return file.exists();
+		
+	}
+	
+	private void copyDatabase() throws IOException {
+		InputStream input = context.getAssets().open(DB_NAME);
+		OutputStream output = new FileOutputStream(DB_PATH + DB_NAME);
+		byte[] buffer = new byte[1024];
+		int lenght;
+		while((lenght = input.read(buffer)) >0) {
+			output.write(buffer, 0, lenght);
+		}
+		output.flush();
+		output.close();
+		input.close();		
+	}
+	
+	@Override
+	public synchronized void close() {
+		if(database != null) {
+			database.close();
+		}
+		super.close();
 	}
 	
 	public TurtleSQLiteDatabase(Context context, String name,
@@ -33,5 +79,15 @@ public class TurtleSQLiteDatabase extends SQLiteOpenHelper {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void openDataBase() throws SQLException{
+		 
+    	//Open the database
+        String myPath = DB_PATH + DB_NAME;
+    	database = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+ 
+    }
+	
+	
 
 }
