19a1a2c6a5bde8d51d3dc512608ada64a30be7e5
Sofia Larsson <karinsofiapaulina@gmail.com>
2013-04-15 20:45:54 +0200

    More appropriate name for helper class

--- TurtleStack/src/com/example/turtlestack/TurtleSQLiteHelper.java
+++ TurtleStack/src/com/example/turtlestack/TurtleSQLiteHelper.java
@@ -8,25 +8,30 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class TurtleSQLiteDatabase extends SQLiteOpenHelper {
+public class TurtleSQLiteHelper extends SQLiteOpenHelper {
 
 	private static String DB_PATH="";
-	private static String DB_NAME="so.sqlite";
-	Context context;
+	private static String DB_NAME="so.db";
+	Context context;	
 	
-	private SQLiteDatabase database;
-	
-	
-	public TurtleSQLiteDatabase (Context context) {
+	public TurtleSQLiteHelper (Context context) {
 		super(context, DB_NAME, null, 1);
-		DB_PATH = context.getFilesDir().getPath() + context.getPackageName() + "/databases/";
+		DB_PATH = context.getFilesDir().getPath() + "/databases/";
 		this.context = context;
 	}
 	
+	/**
+	 * If the database is not already created - create a new database on the device.
+	 */
 	public void createDatabase() {
 		if(!databaseExists()) {
-			this.getReadableDatabase();
-			this.close();
+			File f = new File(DB_PATH);
+			if (!f.exists()) {
+				f.mkdir();
+			}
+			
+			this.getReadableDatabase().close();
+			
 			try {
 				copyDatabase();
 			}
@@ -39,34 +44,28 @@ public class TurtleSQLiteDatabase extends SQLiteOpenHelper {
 	private boolean databaseExists() {
 		File file = new File(DB_PATH + DB_NAME);
 		return file.exists();
-		
 	}
 	
+	/**
+	 * Copy database to DB_PATH+DB_NAME
+	 * @throws IOException
+	 */
 	private void copyDatabase() throws IOException {
 		InputStream input = context.getAssets().open(DB_NAME);
 		OutputStream output = new FileOutputStream(DB_PATH + DB_NAME);
 		byte[] buffer = new byte[1024];
-		int lenght;
-		while((lenght = input.read(buffer)) >0) {
-			output.write(buffer, 0, lenght);
+		int length;
+		while((length = input.read(buffer))>0) {
+			output.write(buffer, 0, length);
 		}
 		output.flush();
 		output.close();
 		input.close();		
 	}
 	
-	@Override
-	public synchronized void close() {
-		if(database != null) {
-			database.close();
-		}
-		super.close();
-	}
-	
-	public TurtleSQLiteDatabase(Context context, String name,
+	public TurtleSQLiteHelper(Context context, String name,
 			CursorFactory factory, int version) {
 		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -76,18 +75,16 @@ public class TurtleSQLiteDatabase extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
 		
 	}
-	
-	public void openDataBase() throws SQLException{
-		 
+	/**
+	 * Open the database defined with DB_PATH and DB_NAME
+	 * @return an opened database
+	 * @throws SQLException
+	 */
+	public SQLiteDatabase openDataBase() throws SQLException{
     	//Open the database
         String myPath = DB_PATH + DB_NAME;
-    	database = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
- 
+    	return SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
     }
-	
-	
-
 }
