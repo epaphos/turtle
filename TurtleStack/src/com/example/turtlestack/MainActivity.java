9f33bcaa08826b9fe76744d759c7b5f19d39b515
Epaphos <epaphos.dev@gmail.com>
2013-04-16 09:50:48 +0200

    Added Activity change
    Added the activity swap for posting question.
    No additional functionality yet.

--- TurtleStack/src/com/example/turtlestack/MainActivity.java
+++ TurtleStack/src/com/example/turtlestack/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.turtlestack;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,6 +19,7 @@ public class MainActivity extends Activity {
 		QuestionDataSource ds = new QuestionDataSource(this);
 		ds.open();
 		Log.v("question", ds.getQuestion(8414075).getTitle());
+		
 	}
 	
 	
@@ -23,5 +29,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, QuestionActivity.class);
+		EditText editText = (EditText) findViewById(R.id.editText1);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 }
