438eedeed2c56468ffc76e1e04da9a34426c908a
Björn Lexell <bjorn.lexell@gmail.com>
2013-04-18 12:15:30 +0200

    Changed SQL query for getting recentposts

--- TurtleStack/src/com/example/turtlestack/PostDataSource.java
+++ TurtleStack/src/com/example/turtlestack/PostDataSource.java
@@ -1,5 +1,7 @@
 package com.example.turtlestack;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.database.*;
 import android.database.sqlite.SQLiteDatabase;
@@ -19,12 +21,25 @@ public class PostDataSource {
 	}
 	
 	public void close() {
-		helper.close();
+		helper.close(); 
 	}
 	
 	public Post getPost(int id) {
 		Cursor cursor = database.rawQuery("select * from posts where id = ?", new String[] { String.valueOf(id) });
 		cursor.moveToFirst();
 		return new Post(cursor.getString(cursor.getColumnIndex("title")));
+	}
+	
+	public ArrayList<Post> getRecentPosts(int numberOfPosts) {
+		//Cursor cursor = database.rawQuery("SELECT * FROM posts p WHERE p.creation_date = (SELECT (creation_date)) LIMIT ?", new String [] {String.valueOf(numberOfPosts)}) ;
+		Cursor cursor = database.rawQuery("SELECT id, post_type_id, title, creation_date FROM posts where post_type_id=1 ORDER BY creation_date DESC LIMIT ?", new String [] {String.valueOf(numberOfPosts)});
+		ArrayList<Post> list = new ArrayList<Post>();
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()) {
+			list.add(new Post(cursor.getString(cursor.getColumnIndex("title"))));
+			cursor.moveToNext();			
+		}
+	
+		return list;
 	}
 }
