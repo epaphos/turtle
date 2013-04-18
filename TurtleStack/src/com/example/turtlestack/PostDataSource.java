40a3855145216ebbce5eb3974280195bf3ee60ab
Björn Lexell <bjorn.lexell@gmail.com>
2013-04-18 14:24:23 +0200

    changed postsourcedata

--- TurtleStack/src/com/example/turtlestack/PostDataSource.java
+++ TurtleStack/src/com/example/turtlestack/PostDataSource.java
@@ -29,17 +29,4 @@ public class PostDataSource {
 		cursor.moveToFirst();
 		return new Post(cursor.getString(cursor.getColumnIndex("title")));
 	}
-	
-	public ArrayList<Post> getRecentPosts(int numberOfPosts) {
-		//Cursor cursor = database.rawQuery("SELECT * FROM posts p WHERE p.creation_date = (SELECT (creation_date)) LIMIT ?", new String [] {String.valueOf(numberOfPosts)}) ;
-		Cursor cursor = database.rawQuery("SELECT id, post_type_id, title, creation_date FROM posts where post_type_id=1 ORDER BY creation_date DESC LIMIT ?", new String [] {String.valueOf(numberOfPosts)});
-		ArrayList<Post> list = new ArrayList<Post>();
-		cursor.moveToFirst();
-		while(!cursor.isAfterLast()) {
-			list.add(new Post(cursor.getString(cursor.getColumnIndex("title"))));
-			cursor.moveToNext();			
-		}
-	
-		return list;
-	}
 }
