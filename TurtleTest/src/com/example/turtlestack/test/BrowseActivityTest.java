795c047cac48c85e5a623dbae80ca124587efa49
Sofia Larsson <karinsofiapaulina@gmail.com>
2013-04-19 16:01:22 +0200

    Test project

---
+++ TurtleTest/src/com/example/turtlestack/test/BrowseActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.turtlestack.test;
+
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+
+import com.example.turtlestack.*;
+
+public class BrowseActivityTest extends ActivityUnitTestCase<BrowseActivity> {
+	QuestionDataSource dataSource;
+	public BrowseActivityTest() {
+		super(BrowseActivity.class);
+	}
+
+}
