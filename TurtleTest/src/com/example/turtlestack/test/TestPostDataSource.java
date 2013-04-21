47b37094ca4ab73c2ccbaeb17a4c9c4f243b9115
Sofia Larsson <karinsofiapaulina@gmail.com>
2013-04-21 22:58:17 +0200

    Create new entry and compare with max id

---
+++ TurtleTest/src/com/example/turtlestack/test/TestPostDataSource.java
@@ -0,0 +1,29 @@
+package com.example.turtlestack.test;
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+import com.example.turtlestack.PostDataSource;
+import com.example.turtlestack.Question;
+
+
+public class TestPostDataSource extends AndroidTestCase {
+
+	PostDataSource datasource;
+
+	protected void setUp() throws Exception {
+        super.setUp();
+		datasource = PostDataSource.getInstance(getContext());
+		datasource.open();		
+    }
+	
+	public void testCompareLastAddedPostWithDatabase() {
+		Question question = new Question("title", "Lorem ipsum dolor sit amet", "tag");
+		datasource.writePost(question);
+		Assert.assertEquals(question.getBody(), datasource.getLastPost().getBody());
+	}
+
+	protected void tearDown() throws Exception {
+		datasource.close();		
+		super.tearDown();
+	}
+}
